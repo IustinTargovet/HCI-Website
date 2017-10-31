04946197905ced71852a255f67b3d1fe6dd54e49
lucywalsh <lucyhannahwalsh@gmail.com>
2017-10-31 09:59:31 +0000

    merged Jess's changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,7 +338,7 @@
 					}
 
 				});
-                
+
                 var slideIndex = 1;
                                 showSlides(slideIndex);
 
@@ -354,15 +354,15 @@
                                   var i;
                                   var slides = document.getElementsByClassName("mySlides");
                                   var dots = document.getElementsByClassName("dot");
-                                  if (n > slides.length) {slideIndex = 1} 
+                                  if (n > slides.length) {slideIndex = 1}
                                   if (n < 1) {slideIndex = slides.length}
                                   for (i = 0; i < slides.length; i++) {
-                                      slides[i].style.display = "none"; 
+                                      slides[i].style.display = "none";
                                   }
                                   for (i = 0; i < dots.length; i++) {
                                       dots[i].className = dots[i].className.replace(" active", "");
                                   }
-                                  slides[slideIndex-1].style.display = "block"; 
+                                  slides[slideIndex-1].style.display = "block";
                                   dots[slideIndex-1].className += " active";
                                 }
 
@@ -417,8 +417,8 @@
 						});
 
 				}
-        
-        
+
+
 
 			// Initialize.
 
